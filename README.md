27429c16bd3a5408281f34e21c4180df65e97a7c
Shirley Silvana Suarez Startary <Shirley Silvana Suarez Startary>
2017-04-05 08:53:05 -0500

    creando funciones de mensaje  y eventos

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-""#Buscaminas"
+"#Buscaminas"
 1. Maquetando la tabla del Buscaminas.
+2. ubicando random las bombas.
+2. se realiza funciones de stopPropagation
--- assets/js/buscaminas.js
+++ assets/js/buscaminas.js
@@ -1,5 +1,43 @@
-document.getElementById("botones").addEventListener("click",buttonClick);
+//document.getElementById("botones").addEventListener("click",buttonClick);
+var arreglosImagenes=[];
 
-function buttonClick(){
-  alert("hola");
+function bombaInit(){
+  var i=0;
+  var random;
+  for(i;i<16;i++){
+    arreglosImagenes[i]="assets/img/punto.jpg"
+  }
+  for(i=0;i<4;i++){
+    do{
+      random=Math.floor(Math.random()*16);
+      //console.log(random);
+
+    }while (arreglosImagenes[random]!="assets/img/punto.jpg")
+
+    arreglosImagenes[random]="assets/img/bomba.jpg"
+  }
+
+}
+
+function buttonClick(imagen){
+  imagen.src=arreglosImagenes[imagen.id];
+  //alert("hola");
+}
+
+var recarga =document.getElementById("recarga");
+recarga.addEventListener("click",recargar);
+function recargar(){
+  location.reload();
+}
+
+document.addEventListener("click", mensaje);
+document.getElementById("tablaBuscaminas").addEventListener("click",noMensaje);
+document.getElementById("recarga").addEventListener("click",noMensaje);
+
+function mensaje(){
+  alert("sigue jugando");
+}
+
+function noMensaje(){
+  event.stopPropagation();
 }
